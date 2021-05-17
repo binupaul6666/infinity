25abbe6f2f23ab55d8970effa7dd61e48912835e
reilynsara <paulbinu1991@gmail.com>
2021-05-17 21:33:41 +0530

    FE changes 17-05-2021

--- src/core/welcome/welcome.js
+++ src/core/welcome/welcome.js
@@ -1,35 +1,72 @@
+import _ from 'underscore';
 import fetchResponse from '/src/services/appServices';
+import animate from "gsap";
 export default {
   name: 'welcome',
   components: {},
   props: [],
   data() {
     return {
-      confirmEmail: '',
+      confirmEmail: false,
+      num_pis: 0
     }
   },
   computed: {
 
   },
   created() {
-    const userId = {
-      userId: this.$route.query.mailId
-    }
-    fetchResponse('put', 'authentication/confirm', userId).then($ => {
-      if ($.data.status) {
-        // action will do in the future
-      } else {
-        this.emitter.emit('error', $.data.message);
+    this.num_pis = 100;
+    if (this.$route.query.mailId !== null && this.$route.query.mailId !== undefined && this.$route.query.mailId !== '') {
+      const userId = {
+        userId: this.$route.query.mailId
       }
-    });
+      fetchResponse('put', 'authentication/confirm', userId).then($ => {
+        if ($.data.status) {
+          this.confirmEmail = true;
+        } else {
+          this.confirmEmail = false;
+          this.emitter.emit('error', $.data.message);
+        }
+      });
+    }
+
   },
   mounted() {
-
-
-
+    this.num_pis = 100;
+    this.animateBlobs();
   },
   methods: {
+    animateBlobs() {
+      let xSeed = _.random(350, 380);
+      let ySeed = _.random(120, 170);
+      let collections = document.getElementsByClassName('blob');
+      for (let i = 0; i < collections.length; i++) {
+        let $blob = collections[i];
+        let speed = _.random(1, 10);
+        let rotation = _.random(5, 100);
+        let scale = _.random(0.8, 1.5);
+        let x =  _.random(-xSeed, xSeed);
+        let y = _.random(-ySeed, ySeed);
 
+        animate.to($blob, {
+          duration: speed,
+          x: x,
+          y: y,
+          ease: "Power2.easeOut",
+          opacity: 0,
+          rotation: rotation,
+          scale: scale,
+          onStartParams: [$blob],
+          onStart: function ($element) {
+            $element.style.display = 'block';
+          },
+          onCompleteParams: [$blob],
+          onComplete: function ($element) {
+            $element.style.display = 'none';
+          }
+        })
+      }
+    }
   }
 }
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import App from './App.vue';
 import router from './router';
 import PrimeVue from 'primevue/config';
 import mitt from 'mitt';
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import ToastService from 'primevue/toastservice';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'primevue/resources/themes/saga-blue/theme.css';
@@ -10,7 +13,10 @@ import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
 import '@/css/style.scss';
 
+library.add(faStar);
+
 const emitter = mitt();
 const app = createApp(App);
 app.config.globalProperties.emitter = emitter;
+app.component('font-awesome-icon', FontAwesomeIcon);
 app.use(router).use(PrimeVue).use(ToastService).mount('#app');
